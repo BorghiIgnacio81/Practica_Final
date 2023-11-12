6a6dc528c875425140f078154049abc19c3702c2
BorghiIgnacio <84923853+BorghiIgnacio@users.noreply.github.com>
2023-11-12 00:24:36 -0300

    00:24 del sabado

--- MVC 6.0/controlador/libros_controlador.php
+++ MVC 6.0/controlador/libros_controlador.php
@@ -3,16 +3,24 @@
     include_once __DIR__ . '/../modelo/Libros_modelo.php';
     set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/../modelo/');
     include_once 'Libros_modelo.php';
-
-
-    //$response =  Libros_Controlador::get_Libros_Controlador(json_decode(file_get_contents('php://input'),true));
+    
     $response = json_decode(file_get_contents('php://input'),true);
-        //print_r($response);
-        if($response){
-            Libros_Controlador::get_Libros_Controlador($response);
+        
+    if($response){
+        Libros_Controlador::get_Libros_Controlador($response);
+    }
+
+    class Libros_Controlador { 
+        static public function get_libro_activo_Controlador($data) { 
+            if ($data["funcion"] == "pedidos") {
+                if (isset($data["data"])) {
+                        $pActivo = $data["data"];
+                        $respuesta = Libros_modelo::get_libro_activo_modelo($pActivo);
+                        
+                        echo json_encode($respuesta);
+                    }
+                }
         }
-    class Libros_Controlador {
-      
         static public function get_Libros_Controlador($data) { 
             switch ($data["funcion"]) {
                 case "search":
@@ -20,35 +28,30 @@
                         $pTitulo = $data["data"];
                         $respuesta = Libros_modelo::get_libros_modelo($pTitulo);
                         
-                       //echo json_encode(["respuesta" => $respuesta]);
-                        //var_dump($respuesta);
                         echo json_encode($respuesta);
-                    } else {
-                        // Manejo de error si no se proporciona el dato de búsqueda
-                        echo json_encode(["error" => "No se proporcionó el dato de búsqueda"]);
                     }
                     break;
             
                 case "add":
-                    
                     if (isset($data["data"])) {
                         $aux = $data["data"];
+
                         $libro = array(
-                        "titulo" => $aux["titulo"],
-                        "idAutor" => $aux["autor"],
-                        "ubicacionBiblioteca" => $aux["ubicacionFisica"],
-                        "editorial" => $aux["editorial"],
-                        "lugarEdicion" => $aux["lugarEdicion"],
-                        "anio" => $aux["anio"],
-                        "serie" => $aux["serie"],
-                        "observaciones" => $aux["observaciones"],
-                        "idMateria" => $aux["materia"]
+                            "titulo" => $aux["titulo"],
+                            "idAutor" => $aux["autor"],
+                            "ubicacionBiblioteca" => $aux["ubicacionFisica"],
+                            "idEditorial" => $aux["editorial"],
+                            "lugarEdicion" => $aux["lugarEdicion"],
+                            "anio" => $aux["anio"],
+                            "serie" => $aux["serie"],
+                            "observaciones" => $aux["observaciones"],
+                            "idMateria" => $aux["materia"]
                         );
                         
                         
                         $respuesta = Libros_modelo::nuevo_libro_modelo($libro);
                         if ($respuesta) {
-                        echo json_encode(array("status"=>"ok"));
+                            echo json_encode(array("status"=>"ok"));
                         }else {
                             echo json_encode(array("status"=>"no"));
                         }
--- MVC 6.0/modelo/Libros_modelo.php
+++ MVC 6.0/modelo/Libros_modelo.php
@@ -1,40 +1,90 @@
 <?php
 include_once 'Conectar.php';
 class Libros_modelo {
-    static public function get_libros_modelo($pTitulo) {          
-       
+    static public function get_libros_modelo($pTitulo) {             
         try {
             $consulta = Conectar::conexion()->prepare("CALL `librosXtitulo`(:pTitulo)");
+
             $consulta->bindParam(":pTitulo", $pTitulo, PDO::PARAM_STR);
+            
             $consulta->execute();
             
             $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
             return $resultados;
          
         } catch (PDOException $e) {
-            echo "Error PDO!!!: " . $e->getMessage();
+            return array();
         }
-    } 
-    static public function nuevo_libro_modelo($libro)
-        {
-        try { //CALL nuevoLibro('AlgoAAAAAAAAAAAAAAAAAA',1,1,'A1','a',1,1,'a',1);
-        $consulta = Conectar::conexion()->prepare("CALL `nuevoLibro`(:titulo, :idAutor, :idEditorial, :ubicacionBiblioteca, :lugarEdicion, :anio, :serie, :observaciones, :idMateria)");
-        // UN ENLACE POR CADA DATO, TENER EN CUENTA EL TIPO DE DATO STR O INT
-        $consulta->bindParam(":titulo", $libro["titulo"], PDO::PARAM_STR);
-        $consulta->bindParam(":idAutor", $libro["idAutor"], PDO::PARAM_INT);
-        $consulta->bindParam(":idEditorial", $libro["idEditorial"], PDO::PARAM_INT);
-        $consulta->bindParam(":ubicacionBiblioteca", $libro["ubicacionBiblioteca"], PDO::PARAM_STR);
-        $consulta->bindParam(":lugarEdicion", $libro["lugarEdicion"], PDO::PARAM_STR);
-        $consulta->bindParam(":anio", $libro["anio"], PDO::PARAM_INT);
-        $consulta->bindParam(":serie", $libro["serie"], PDO::PARAM_INT);
-        $consulta->bindParam(":observaciones", $libro["observaciones"], PDO::PARAM_STR);
-        $consulta->bindParam(":idMateria", $libro["idMateria"], PDO::PARAM_INT);
-        $consulta->execute();
-        return true;
+    }
+    static public function get_libro_activo_modelo($pActivo) {             
+        try {
+            $consulta = Conectar::conexion()->prepare("CALL `librosXactivo`(:pActivo)");
+
+            $consulta->bindParam(":pActivo", $pActivo, PDO::PARAM_STR);
+            
+            $consulta->execute();
+            
+            $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
+            return $resultados;
+         
+        } catch (PDOException $e) {
+            return array();
+        }
+    }  
+
+    static public function nuevo_libro_modelo($libro){
+        try {
+            $consulta = Conectar::conexion()->prepare("CALL `nuevoLibro`(
+                :titulo,
+                :idAutor,
+                :idEditorial, 
+                :ubicacionBiblioteca, 
+                :lugarEdicion, 
+                :anio, 
+                :serie, 
+                :observaciones, 
+                :idMateria)");
+
+            // UN ENLACE POR CADA DATO, TENER EN CUENTA EL TIPO DE DATO STR O INT
+            
+            $consulta->bindParam(":titulo", $libro["titulo"], PDO::PARAM_STR);
+            $consulta->bindParam(":idAutor", $libro["idAutor"], PDO::PARAM_INT);
+            $consulta->bindParam(":idEditorial", $libro["idEditorial"], PDO::PARAM_INT);
+            $consulta->bindParam(":ubicacionBiblioteca", $libro["ubicacionBiblioteca"], PDO::PARAM_STR);
+            $consulta->bindParam(":lugarEdicion", $libro["lugarEdicion"], PDO::PARAM_STR);
+            $consulta->bindParam(":anio", $libro["anio"], PDO::PARAM_INT);
+            $consulta->bindParam(":serie", $libro["serie"], PDO::PARAM_INT);
+            $consulta->bindParam(":observaciones", $libro["observaciones"], PDO::PARAM_STR);
+            $consulta->bindParam(":idMateria", $libro["idMateria"], PDO::PARAM_INT);
+
+            $consulta->execute();
+            
+            return true;
         
         } catch (Exception $e) {
-        return "Error: " . $e->getMessage();
+            print_r($e);
+            return false;
         }
-    }   
+    }
+
+    // static public function editar_libro_modelo($libro)
+    // {
+    //     try 
+    //     {
+    //         $stmt = Conectar::conexion()->prepare("UPDATE $tabla SET dato1 = :dato1, …. WHERE 
+    //         id_dato = :id_dato");
+    
+    //         $stmt->bindParam(":dato1", $libro[“dato1”], PDO::PARAM_STR);
+    //         $stmt->bindParam(":id_dato", $libro[“id_dato”], PDO::PARAM_INT);
+    //         if ($stmt->execute()) {
+    //             return "ok";
+    //         } else {
+    //         return print_r(Conectar::conexion()->errorInfo());
+    //         }
+    //     } 
+    //     catch (Exception $e) {
+    //         return "Error: " . $e->getMessage();
+    //     }
+    // } 
 }
 ?>
