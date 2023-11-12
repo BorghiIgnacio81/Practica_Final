4128fa744eb8f07047db235c297379a43a4b6f9e
BorghiIgnacio <84923853+BorghiIgnacio@users.noreply.github.com>
2023-11-12 06:34:36 -0300

    DB correcciones

--- MVC 6.0/modelo/Libros_modelo.php
+++ MVC 6.0/modelo/Libros_modelo.php
@@ -67,24 +67,46 @@ class Libros_modelo {
         }
     }
 
-    // static public function editar_libro_modelo($libro)
-    // {
-    //     try 
-    //     {
-    //         $stmt = Conectar::conexion()->prepare("UPDATE $tabla SET dato1 = :dato1, …. WHERE 
-    //         id_dato = :id_dato");
+    static public function editar_libro_modelo($libro)
+    {
+        try 
+        {
+            $consulta = Conectar::conexion()->prepare(" CALL `editarLibro`(
+                :idLibro,
+                :titulo,
+                :idAutor,
+                :idEditorial, 
+                :ubicacionBiblioteca, 
+                :lugarEdicion, 
+                :anio, 
+                :serie, 
+                :observaciones, 
+                :idMateria)");
+                $consulta->bindParam(":idLibro", $libro["idLibro"], PDO::PARAM_STR); //
+                $consulta->bindParam(":titulo", $libro["titulo"], PDO::PARAM_STR);
+                $consulta->bindParam(":idAutor", $libro["idAutor"], PDO::PARAM_INT);
+                $consulta->bindParam(":idEditorial", $libro["idEditorial"], PDO::PARAM_INT);
+                $consulta->bindParam(":ubicacionBiblioteca", $libro["ubicacionBiblioteca"], PDO::PARAM_STR);
+                $consulta->bindParam(":lugarEdicion", $libro["lugarEdicion"], PDO::PARAM_STR);
+                $consulta->bindParam(":anio", $libro["anio"], PDO::PARAM_INT);
+                $consulta->bindParam(":serie", $libro["serie"], PDO::PARAM_INT);
+                $consulta->bindParam(":observaciones", $libro["observaciones"], PDO::PARAM_STR);
+                $consulta->bindParam(":idMateria", $libro["idMateria"], PDO::PARAM_INT);
     
-    //         $stmt->bindParam(":dato1", $libro[“dato1”], PDO::PARAM_STR);
-    //         $stmt->bindParam(":id_dato", $libro[“id_dato”], PDO::PARAM_INT);
-    //         if ($stmt->execute()) {
-    //             return "ok";
-    //         } else {
-    //         return print_r(Conectar::conexion()->errorInfo());
-    //         }
-    //     } 
-    //     catch (Exception $e) {
-    //         return "Error: " . $e->getMessage();
-    //     }
-    // } 
+                $consulta->execute();
+                $resultados = $consulta->fetch(PDO::FETCH_ASSOC);
+                return $resultados;
+                
+                
+            if ($consulta->execute()) {
+                return "ok";
+            } else {
+            return print_r(Conectar::conexion()->errorInfo());
+            }
+        } 
+        catch (Exception $e) {
+            return "Error: " . $e->getMessage();
+        }
+    } 
 }
 ?>
